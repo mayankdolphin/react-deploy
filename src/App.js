94298087840fde3a0ffff97b21044b4dbe512eb9
Mayank Zalavadia <mayankzalavadia@gmail.com>
2020-04-21 12:59:11 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Github<code>src/App.js</code> and save to reload.
+          Edit MK <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
