794174c16cd9df8d3323dcc6bc942155f70ac21c
Mayank Zalavadia <mayankzalavadia@gmail.com>
2020-04-21 12:55:08 +0530

    Updates

---
+++ precache-manifest.be29ac231448dee4ecf4429d9052c5e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11a3a76180422b4e7546f2f38ece1b5",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "7e994d50a992b2464f5e",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0413c559a72e55e4719f",
+    "url": "/react-deploy/static/js/2.6e74cc94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.6e74cc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e994d50a992b2464f5e",
+    "url": "/react-deploy/static/js/main.1327c898.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
